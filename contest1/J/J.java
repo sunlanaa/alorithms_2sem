eb991fae21c5357dbe07e5b51373f3935a5f0bfa
svetlana <ivanova.sd@phystech.edu>
2023-04-25 14:57:58 +0300

    fixed

--- contest1/J/J.java
+++ contest1/J/J.java
@@ -1,8 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.Objects;
 
 class Deque {
     private int capacity;
@@ -11,8 +9,8 @@ class Deque {
     private int front;
     private int tail;
 
-    public Deque(int capacity) {
-        this.capacity = Math.max(capacity, 1);
+    public Deque() {
+        this.capacity = 3;
         this.data = new int[this.capacity];
         this.front = -1;
         this.tail = -1;
@@ -27,7 +25,25 @@ class Deque {
         return size == 0;
     }
 
+    public void resize() {
+        int[] newData = new int[capacity * 2];
+        front = tail + 1;
+        for (int i = 0; i < capacity; i++) {
+            if (front == 0) {
+                front = capacity * 2 - 1;
+            } else {
+                front--;
+            }
+            newData[front] = data[front % capacity];
+        }
+        data = newData;
+        capacity *= 2;
+    }
+
     public void pushFront(int element) { // boolean
+        if (isFull()) {
+            resize();
+        }
         if (isEmpty()) {
             front = tail = 0;
             data[0] = element;
@@ -38,10 +54,12 @@ class Deque {
             data[--front] = element;
         }
         size++;
-        System.out.println("ok");
     }
 
     public void pushBack(int element) {
+        if (isFull()) {
+            resize();
+        }
         if (isEmpty()) {
             front = tail = 0;
             data[tail] = element;
@@ -52,7 +70,6 @@ class Deque {
             data[++tail] = element;
         }
         size++;
-        System.out.println("ok");
     }
 
     public int popFront() {
@@ -60,6 +77,10 @@ class Deque {
         data[front] = 0;
         if (front == capacity - 1) {
             front = 0;
+        } else if (ans == 0) {
+            front += capacity - size;
+            ans = data[front];
+            data[front] = 0;
         } else {
             front++;
         }
@@ -71,11 +92,12 @@ class Deque {
         int ans = data[tail];
         data[tail] = 0;
         if (tail == 0) {
-            tail = capacity - 1;
+            tail = Math.abs(capacity) - 1;
+        } else if (ans == 0) {
+            tail -= capacity - size;
         } else {
             tail--;
         }
-
         size--;
         return ans;
     }
@@ -93,65 +115,71 @@ class Deque {
     }
 
     public void clear() {
-        int[] data = new int[capacity];
         front = -1;
-        tail = 0;
+        tail = -1;
         size = 0;
-        System.out.println("ok");
-    }
-
-    public void exit() {
-        System.out.println("bye");
-    }
-
-
-    public void print() {
-//        if (front <= tail) {
-//            for (int i = front; i < tail + 1; i++) {
-//                System.out.print(data[i] + " ");
-//            }
-//        } else {
-//            for (int i = front; i < capacity; i++) {
-//                System.out.print(data[i] + " ");
-//            }
-//            for (int i = 0; i < tail + 1; i++) {
-//                System.out.print(data[i] + " ");
-//            }
-//        }
-//        System.out.println();
-        System.out.println(Arrays.toString(data));
     }
 
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         String count = reader.readLine();
-        Deque deque1 = new Deque(Integer.parseInt(count));
+        Deque deque1 = new Deque();
 
         for (int i = 0; i < Integer.parseInt(count); i++) {
             String str = reader.readLine();
             String[] command = str.split(" ");
-            if (Objects.equals(command[0], "push_front")) {
-                deque1.pushFront(Integer.parseInt(command[1]));
-            } else if (Objects.equals(command[0], "push_back")) {
-                deque1.pushBack(Integer.parseInt(command[1]));
-            } else if (Objects.equals(command[0], "pop_front") && (!deque1.isEmpty())) {
-                System.out.println(deque1.popFront());
-            } else if (Objects.equals(command[0], "pop_back") && (!deque1.isEmpty())) {
-                System.out.println(deque1.popBack());
-            } else if (Objects.equals(command[0], "front") && (!deque1.isEmpty())) {
-                System.out.println(deque1.front_el());
-            } else if (Objects.equals(command[0], "back") && (!deque1.isEmpty())) {
-                System.out.println(deque1.back_el());
-            }else if (Objects.equals(command[0], "size")) {
-                System.out.println(deque1.getSize());
-            } else if (Objects.equals(command[0], "clear")) {
-                deque1.clear();
-            } else if (Objects.equals(command[0], "exit")) {
-                deque1.exit();
-                break;
-            } else{
-                System.out.println("error");
+            switch (command[0]) {
+                case "push_front":
+                    deque1.pushFront(Integer.parseInt(command[1]));
+                    System.out.println("ok");
+                    break;
+                case "push_back":
+                    deque1.pushBack(Integer.parseInt(command[1]));
+                    System.out.println("ok");
+                    break;
+                case "pop_front":
+                    if (!deque1.isEmpty()) {
+                        System.out.println(deque1.popFront());
+                    } else {
+                        System.out.println("error");
+                    }
+                    break;
+                case "pop_back":
+                    if (!deque1.isEmpty()) {
+                        System.out.println(deque1.popBack());
+                    } else {
+                        System.out.println("error");
+                    }
+                    break;
+                case "front":
+                    if (!deque1.isEmpty()) {
+                        System.out.println(deque1.front_el());
+                    } else {
+                        System.out.println("error");
+                    }
+                    break;
+                case "back":
+                    if (!deque1.isEmpty()) {
+                        System.out.println(deque1.back_el());
+                    } else {
+                        System.out.println("error");
+                    }
+                    break;
+                case "size":
+                    System.out.println(deque1.getSize());
+                    break;
+                case "clear":
+                    deque1.clear();
+                    System.out.println("ok");
+                    break;
+                case "exit":
+                    System.out.println("bye");
+                    return;
+                default:
+                    System.out.println("error");
+                    break;
             }
         }
     }
 }
+
