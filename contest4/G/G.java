7ae809d713263a3ab327aee16b864c11676b256b
svetlana <ivanova.sd@phystech.edu>
2023-06-13 19:03:25 +0300

    fixed

--- contest4/G/G.java
+++ contest4/G/G.java
@@ -1,42 +1,55 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+import java.util.Stack;
+
+import javax.xml.catalog.Catalog;
+
 import java.util.*;
 
-class G {
-    public static boolean dfsik(int num, List<Integer> used, Stack<Integer> vertexes,
+enum VertexStatus {
+    WHITE,
+    GREY,
+    BLACK
+}
+
+class TaskG {
+    public static boolean dfsik(int num, List<VertexStatus> used, Stack<Integer> vertexes,
                                 List<List<Integer>> neighbours) {
-        used.set(num, 1);
-        boolean det = false;
+        used.set(num, VertexStatus.GREY);
+        boolean isPossibletoSort = false;
         for (int i = 0; i < neighbours.get(num).size(); ++i) {
-            if (used.get(neighbours.get(num).get(i)) == 0) {
-                det = dfsik(neighbours.get(num).get(i), used, vertexes, neighbours);
-            } else if (used.get(neighbours.get(num).get(i)) == 1) {
-                det = true;
+            int neighbour = neighbours.get(num).get(i);
+            if (used.get(neighbour) == VertexStatus.WHITE) {
+                isPossibletoSort = dfsik(neighbour, used, vertexes, neighbours);
+            } else if (used.get(neighbour) == VertexStatus.GREY) {
+                isPossibletoSort = true;
             }
-            if (det) {
-                return det;
+            if (isPossibletoSort) {
+                return isPossibletoSort;
             }
         }
         vertexes.push(num);
-        used.set(num, 2);
-        return det;
+        used.set(num, VertexStatus.BLACK);
+        return isPossibletoSort;
     }
 
-    public static void dfs(List<Integer> used, Stack<Integer> vertexes,
+    public static void dfs(List<VertexStatus> used, Stack<Integer> vertexes,
                            List<List<Integer>> neighbours) {
-        boolean det = false;
+        boolean isPossibletoSort = false;
         for (int i = 0; i < used.size(); ++i) {
-            if (used.get(i) == 0) {
-                det = dfsik(i, used, vertexes, neighbours);
-                if (det) {
+            if (used.get(i) == VertexStatus.WHITE) {
+                isPossibletoSort = dfsik(i, used, vertexes, neighbours);
+                if (isPossibletoSort) {
                     System.out.println("-1");
-                    break;
+                    return;
                 }
             }
         }
-        if (!det) {
-            while (!vertexes.empty()) {
-                System.out.print((vertexes.peek() + 1) + " ");
-                vertexes.pop();
-            }
+        while (!vertexes.empty()) {
+            System.out.print((vertexes.peek() + 1) + " ");
+            vertexes.pop();
         }
     }
 
@@ -50,32 +63,44 @@ class G {
             int second = connects.get(i).getR() - 1;
             neighbours.get(first).add(second);
         }
-        List<Integer> used = new ArrayList<>(Collections.nCopies(number, 0));
+        List<VertexStatus> used = new ArrayList<>(Collections.nCopies(number, VertexStatus.WHITE));
         Stack<Integer> vertexes = new Stack<>();
         dfs(used, vertexes, neighbours);
     }
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int number = scanner.nextInt();
-        int count = scanner.nextInt();
-        List<Pair<Integer, Integer>> connects = new ArrayList<>();
-        for (int i = 0; i < count; ++i) {
-            int elem_a = scanner.nextInt();
-            int elem_b = scanner.nextInt();
-            Pair<Integer, Integer> new_pair = new Pair<>(elem_a, elem_b);
-            connects.add(new_pair);
+        try (Scanner scanner = new Scanner(System.in)) {
+            int number = scanner.nextInt();
+            int count = scanner.nextInt();
+            List<Pair<Integer, Integer>> connects = new ArrayList<>();
+            for (int i = 0; i < count; ++i) {
+                int elem_a = scanner.nextInt();
+                int elem_b = scanner.nextInt();
+                Pair<Integer, Integer> new_pair = new Pair<>(elem_a, elem_b);
+                connects.add(new_pair);
+            }
+            worker(number, connects);
+        } catch (Exception ex) {
+            System.out.println(ex);
         }
-        worker(number, connects);
+        
     }
 }
-class Pair<L,R> {
+
+class Pair<L, R> {
     private L l;
     private R r;
-    public Pair(L l, R r){
+
+    public Pair(L l, R r) {
         this.l = l;
         this.r = r;
     }
-    public L getL(){ return l; }
-    public R getR(){ return r; }
+
+    public L getL() {
+        return l;
+    }
+
+    public R getR() {
+        return r;
+    }
 }
